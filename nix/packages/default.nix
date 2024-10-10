35f8b65ad42ddb259a1da9710d57daa232fcfa6c
nikstur <nikstur@outlook.com>
2024-10-10 22:45:22 +0200

    flake: add cross and static build

--- nix/packages/default.nix
+++ nix/packages/default.nix
@@ -2,4 +2,6 @@
 
 {
   userborn = pkgs.callPackage ./userborn.nix { };
+  static = pkgs.pkgsStatic.callPackage ./userborn.nix { };
+  cross = pkgs.pkgsCross.aarch64-multiplatform.callPackage ./userborn.nix { };
 }
